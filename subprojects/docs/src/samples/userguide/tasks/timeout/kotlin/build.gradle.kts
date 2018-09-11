747856cbb6bd913d9b9ee2ef61e39c353c915b10
Stefan Oehme <stefan@gradle.com>
2018-09-11 11:06:37 +0200

    Document task timeouts in user guide

---
+++ subprojects/docs/src/samples/userguide/tasks/timeout/kotlin/build.gradle.kts
@@ -0,0 +1,11 @@
+import java.time.Duration
+
+tasks {
+    register("hangingTask") {
+        doLast {
+            Thread.sleep(100000)
+        }
+
+        timeout.set(Duration.ofMillis(500))
+    }
+}
